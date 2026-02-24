4e932ebdeaa9a44469c04c7be0b25997db6481a8
DereckAn <dereckangeles15071998@gmail.com>
2026-02-24 02:10:48 -0600

    Cleaning repo

--- libs/utils.tsx
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"; 
-
-export function cn(...classes: ClassValue[]) {
-    return twMerge(clsx(classes));
-}
-
-//note: este es un gist que sirve para hacer mas interactiva las classes
-// solo cambiar 
