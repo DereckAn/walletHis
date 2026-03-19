8ca07b8228cd4a7379e7ac5d94487418a209ba80
DereckAn <dereckangeles15071998@gmail.com>
2026-03-19 10:04:56 -0600

    new apple poroject

---
+++ walletHis/walletHis/walletHisApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletHisApp.swift
+//  walletHis
+//
+//  Created by La ruina on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct walletHisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ walletHisTests/walletHisTests.swift
@@ -0,0 +1,16 @@
+//
+//  walletHisTests.swift
+//  walletHisTests
+//
+//  Created by La ruina on 4/5/26.
+//
+
+import Testing
+
+struct walletHisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
